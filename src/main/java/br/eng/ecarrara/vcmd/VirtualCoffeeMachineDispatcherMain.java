5edc6c12109ff51a9f981be470b4c122b00d0d3b
Eduardo Carrara <ecarrara.araujo@gmail.com>
2016-02-15 22:25:22 -0200

    Basic gradle infrastructure with a Main java entry point.
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

---
+++ src/main/java/br/eng/ecarrara/vcmd/VirtualCoffeeMachineDispatcherMain.java
@@ -0,0 +1,8 @@
+package br.eng.ecarrara.vcmd;
+
+public class VirtualCoffeeMachineDispatcherMain {
+    
+    public static void main(String[] args) {
+        System.out.println("Coffee Machine Not Working Yet - Come back later!");
+    }
+}
